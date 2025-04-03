e7bdadd77570743ad9a5b0c4ad5f99391164fa09
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:26:49 +0330

    add model core

--- services/models.py
+++ services/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-
-class Service(models.Model):
-    title = models.CharField(max_length=200, verbose_name="عنوان خدمت")
-    description = models.TextField(verbose_name="توضیحات")
-    image = models.ImageField(upload_to='services/', verbose_name="تصویر")
-    icon = models.CharField(max_length=50, verbose_name="آیکون", help_text="نام کلاس FontAwesome", blank=True)
-    order = models.PositiveIntegerField(default=0, verbose_name="ترتیب نمایش")
-
-    class Meta:
-        verbose_name = "خدمت"
-        verbose_name_plural = "خدمات"
-        ordering = ['order']
-
-    def __str__(self):
-        return self.title
+# Create your models here.
