28029da2e1f339a94edf6becdba06a629d720729
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:40:34 +0330

    add view for core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import SiteSettings
+from services.models import Service
+from projects.models import Project
 
-# Create your views here.
+
+def index(request):
+    site_settings = SiteSettings.objects.first()
+    services = Service.objects.all()[:6]
+    latest_projects = Project.objects.all()[:3]
+
+    context = {
+        'site_settings': site_settings,
+        'services': services,
+        'latest_projects': latest_projects,
+    }
+    return render(request, 'core/index.html', context)
+
+
+def about(request):
+    site_settings = SiteSettings.objects.first()
+
+    context = {
+        'site_settings': site_settings,
+    }
+    return render(request, 'core/about.html', context)
