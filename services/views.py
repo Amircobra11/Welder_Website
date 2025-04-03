26d13f4e2a4d7d0fae2a46cbbf8da9fe693eeca2
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:46:32 +0330

    add view for services

--- services/views.py
+++ services/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Service
+from core.models import SiteSettings
 
-# Create your views here.
+
+def service_list(request):
+    services = Service.objects.all()
+    site_settings = SiteSettings.objects.first()
+
+    context = {
+        'services': services,
+        'site_settings': site_settings,
+    }
+    return render(request, 'services/service_list.html', context)
