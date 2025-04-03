716f794f19a9978688758e11172f79460343c77b
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:30:44 +0330

    add admin contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'created_at', 'is_read')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('name', 'email', 'subject', 'message')
+    list_editable = ('is_read',)
