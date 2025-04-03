2aa36ee7bc4da35c3de38b2a2baa65f6af58a92b
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:43:49 +0330

    add view for projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+from core.models import SiteSettings
 
-# Create your views here.
+
+def project_list(request):
+    projects = Project.objects.all()
+    site_settings = SiteSettings.objects.first()
+
+    context = {
+        'projects': projects,
+        'site_settings': site_settings,
+    }
+    return render(request, 'projects/project_list.html', context)
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    site_settings = SiteSettings.objects.first()
+
+    context = {
+        'project': project,
+        'site_settings': site_settings,
+    }
+    return render(request, 'projects/project_detail.html', context)
