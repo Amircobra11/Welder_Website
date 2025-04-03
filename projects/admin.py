8e66cd759694aaa8276e82a2449e2dbeccb2c766
amir <amir.sisi.cobra11.com@gmail.com>
2025-04-03 10:32:24 +0330

    add admin project

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, ProjectImage
 
-# Register your models here.
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 3
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'completion_date')
+    list_filter = ('completion_date',)
+    search_fields = ('title', 'client', 'location')
+    inlines = [ProjectImageInline]
